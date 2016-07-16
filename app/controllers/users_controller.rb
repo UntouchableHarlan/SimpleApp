00c45163212d2b093a9ebcdf77f3a54e2eee73a8
Salomon <salomonpluviose@yahoo.com>
2016-07-16 15:41:32 -0400

    Added more user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,27 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Successfully updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def show
@@ -12,11 +33,24 @@ class UsersController < ApplicationController
     if @user.save
       log_in @user
       flash[:success] = "Welcome to the Sample App, #{@user.name}!"
-      redirect_to @user
+      redirect_back_or @user
       else
         render 'new'
       end
   end
+
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 
   private
 
